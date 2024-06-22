c5242ecec798b83ac2fda93159ca9726c19b5f26
Mehedi Nayem <naembfh@gmail.com>
2024-06-22 20:25:42 +0800

    error

--- dist/server.js
+++ dist/server.js
@@ -15,12 +15,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 const app_1 = __importDefault(require("./app"));
 const config_1 = __importDefault(require("./app/config"));
+let server;
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            console.log(config_1.default.database_url);
             yield mongoose_1.default.connect(config_1.default.database_url);
-            app_1.default.listen(config_1.default.port, () => {
+            server = app_1.default.listen(config_1.default.port, () => {
                 console.log(`app is listening on port ${config_1.default.port}`);
             });
         }
@@ -30,3 +30,16 @@ function main() {
     });
 }
 main();
+process.on("unhandledRejection", () => {
+    console.log(`😈 unahandledRejection is detected , shutting down ...`);
+    if (server) {
+        server.close(() => {
+            process.exit(1);
+        });
+    }
+    process.exit(1);
+});
+process.on("uncaughtException", () => {
+    console.log(`😈 uncaughtException is detected , shutting down ...`);
+    process.exit(1);
+});
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
+import { Server } from "http";
 import mongoose from "mongoose";
 import app from "./app";
 import config from "./app/config";
+let server: Server;
 
 async function main() {
   try {
-    console.log(config.database_url);
-
     await mongoose.connect(config.database_url as string);
 
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(`app is listening on port ${config.port}`);
     });
   } catch (err) {
@@ -17,3 +17,18 @@ async function main() {
 }
 
 main();
+
+process.on("unhandledRejection", () => {
+  console.log(`😈 unahandledRejection is detected , shutting down ...`);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on("uncaughtException", () => {
+  console.log(`😈 uncaughtException is detected , shutting down ...`);
+  process.exit(1);
+});
